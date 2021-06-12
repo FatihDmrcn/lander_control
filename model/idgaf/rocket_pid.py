c656e60cb7fb309c38498a24e0abc4ddfd759b27
FatihDmrcn <27896339+FatihDmrcn@users.noreply.github.com>
2021-06-12 15:32:12 +0200

    attitude controller added

--- model/idgaf/rocket_pid.py
+++ model/idgaf/rocket_pid.py
@@ -21,19 +21,60 @@ def pid_thrust(_force, _y_desired, _y_actual):
     return f
 
 
-KP = np.zeros((3, 12))
-KI = np.zeros((3, 12))
-KD = np.zeros((3, 12))
+KP_thr = np.zeros((3, 12))
+KI_thr = np.zeros((3, 12))
+KD_thr = np.zeros((3, 12))
 
-KP[2, 2] = 150
-KI[2, 2] = 0.1
-KD[2, 2] = 3000
+KP_thr[2, 2] = 150
+KI_thr[2, 2] = 0.1
+KD_thr[2, 2] = 3000
 
-KP[2, 8] = 100
-KI[2, 8] = .0
-KD[2, 8] = 15
+KP_thr[2, 8] = 100
+KI_thr[2, 8] = .0
+KD_thr[2, 8] = 15
 
 pid_thrust.min_thrust = 0
 pid_thrust.max_thrust = 1000
-pid_thrust.K = {'P': KP, 'I': KI, 'D': KD}
+pid_thrust.K = {'P': KP_thr, 'I': KI_thr, 'D': KD_thr}
 pid_thrust.e = {'t-1': np.zeros(12), 't-2': np.zeros(12)}
+
+
+def pid_attitude(_force, _y_desired, _y_actual):
+    error = _y_desired - _y_actual
+    _ep = error - pid_attitude.e['t-1']
+    _ei = error + pid_attitude.e['t-1']
+    _ed = np.zeros(12)
+    if np.any(pid_attitude.e['t-2'] != 0):
+        _ed = error - 2 * pid_attitude.e['t-1'] + pid_attitude.e['t-2']
+    print(_ep[4], _ei[4], _ed[4])
+    _d_force = np.dot(pid_attitude.K['P'], _ep) + np.dot(pid_attitude.K['I'], _ei) + np.dot(pid_attitude.K['D'], _ed)
+    f = _force + _d_force
+
+    if np.all(pid_attitude.e['t-2'] == 0):
+        f[0] = 0
+    if f[0] < pid_attitude.min_thrust:
+        f[0] = pid_attitude.min_thrust
+    if f[0] > pid_attitude.max_thrust:
+        f[0] = pid_attitude.max_thrust
+
+    pid_attitude.e['t-2'] = pid_attitude.e['t-1']
+    pid_attitude.e['t-1'] = error
+    return f
+
+
+KP_att = np.zeros((3, 12))
+KI_att = np.zeros((3, 12))
+KD_att = np.zeros((3, 12))
+
+KP_att[0, 4] = -100
+KI_att[0, 4] = -0.1
+KD_att[0, 4] = -10000
+
+KP_att[0, 10] = 0
+KI_att[0, 10] = 0
+KD_att[0, 10] = 0
+
+pid_attitude.min_thrust = -50
+pid_attitude.max_thrust = +50
+pid_attitude.K = {'P': KP_att, 'I': KI_att, 'D': KD_att}
+pid_attitude.e = {'t-1': np.zeros(12), 't-2': np.zeros(12)}
--- model/model.py
+++ model/model.py
@@ -1,7 +1,11 @@
 from idgaf.rocket_model import rocket_eq
-from idgaf.rocket_pid import pid_thrust
+from idgaf.rocket_pid import pid_attitude, pid_thrust
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def plot_telemetry():
+    pass
 
 
 mass = 20.
@@ -10,9 +14,10 @@ radius = 3.
 inertia = 0.25*mass*(radius**2 + (1/3)*length**2)
 forces = np.zeros(3)
 
-y0 = [0., 0., 800., 0., 0., 0., 0., 0., 50., 0., 0., 0.]
+y0 = [0., 0., 1500., 0., -np.pi, 0., 0., 0., -50., 0., 0., 0.]
 y = y0
 y_d = np.zeros(12)
+y_d[2] = length/2.
 
 y_log = [y0]
 f_log = [forces]
@@ -20,8 +25,9 @@ t_step = .01
 t_total = 5000
 
 for t in range(5000):
-    forces = pid_thrust(forces, y_d, y)
+    forces = pid_attitude(forces, y_d, y)
     y = y + t_step*rocket_eq(t, y, forces, mass, length, inertia)
+    print(forces)
     y_log.append(y)
     f_log.append(forces)
 
@@ -31,10 +37,18 @@ t_axis = np.linspace(t_begin, t_end, t_total+1)
 y_log = np.asarray(y_log)
 f_log = np.asarray(f_log)
 
-plt.xlim(t_begin, t_end)
-plt.plot(t_axis, y_log[:, 2], label='alt')
-plt.plot(t_axis, y_log[:, 8], label='vel')
-plt.plot(t_axis, f_log[:, 2], label='force_z')
-plt.grid(True)
-plt.legend()
+fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+fig.suptitle('Aligning x-axis using sharex')
+# ax1.plot(t_axis, y_log[:, 2], label='alt')
+ax1.plot(t_axis, y_log[:, 4], label='alt')
+ax1.set_xlim(t_begin, t_end)
+ax1.grid(True)
+
+# ax2.plot(t_axis, y_log[:, 8], label='vel')
+ax2.plot(t_axis, y_log[:, 10], label='vel')
+ax2.grid(True)
+
+# ax3.plot(t_axis, f_log[:, 2], label='force')
+ax3.plot(t_axis, f_log[:, 0], label='force')
+ax3.grid(True)
 plt.show()
